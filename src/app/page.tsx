9e4a03691158f805f36f75080bacda2efcf59e2b
Pedro <pedroaugustor03@gmail.com>
2024-10-16 17:53:44 -0300

    primeiras mudanças

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,128 @@
-import Image from "next/image";
+import React, { useState, useEffect } from "react";
+import "./index.css";
+import pokelogo from "./assets/pokelogo.png";
 
-export default function Home() {
+
+export default function App() 
+{
+  const [data, setData] = useState('');
+  const [pokemonName, setPokemonName] = useState("");
+  const [pokemonshiny , setPokemonshiny] = useState(false);
+
+
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+        );
+        const jsonData = await response.json();
+        setData(jsonData);
+        console.log(response.status);
+      } catch (error) {
+        console.error("Erro ao buscar dados:", error);
+        
+    
+        // throw new Error ('404')
+      }
+    };
+
+
+    useEffect(() => {
+      fetchData();
+    }, [pokemonName]);
+  
+    const handleInputChange = (event) => {
+      const name = event.target.value;
+      setPokemonName(name.toLowerCase());
+        fetchData();
+    };
+
+
+
+
+  const typeColors = {
+
+    fire: "orangered",
+    water: "dodgerblue",
+    grass: "green",
+    flying: "gray", // Se quiser adicionar mais tipos, basta seguir o mesmo padrão
+    ice: "lightskyblue", 
+    fairy: "lightpink", 
+    dark: "darkslateblue",
+    poison: "purple", 
+     ground: "brown", 
+    psychic: "rgb(228, 177, 185)", 
+     bug: "lightgreen", 
+     ghost: "darkorchid", 
+    steel: "darkgrey", 
+     dragon: "coral", 
+     rock: "darkslategrey", 
+     electric: "gold",
+     fighting: "goldenrod",
+    
+
+    
+  };
+  
+
+
+  const saberTipo = (verify) => {
+    if (verify && typeColors.hasOwnProperty(verify)) {
+      return typeColors[verify];
+    } else {
+      return "white";
+    }
+  };
+  
+
+  const Imageshiny = data?.sprites?.front_shiny || pokelogo;
+  const actualImage = data?.sprites?.front_default  || pokelogo  ;
+  const verify = data.types && data.types.length > 0 && data.types[0].type && data.types[0].type.name || data.types && data.types.length > 0 && data.types[1].type && data.types[1].type.name;
+
+  const habilidade = data.abilities && data.abilities.length > 0 && data.abilities[0].ability && data.abilities[0].ability.name ;
+
+  
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="container">
+      <div className="texto">Digite o nome do Pokémon desejado: </div>
+      <div class="card" style={{
+        backgroundColor:(saberTipo(verify) ) }}>
+        <div className="header-card">
+          <div className="input-area">
+            <input type="text" onChange={handleInputChange} />
+            <div className="tipo">Tipo: {verify ? data.types[0].type.name : '' || verify ? data.types[1].type.name : ''}
+              
+            </div>
+          </div>
+        
+         <div className="card-image ">
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    
+         <img
+           alt={pokemonshiny ? "Shiny" : "Normal"}
+           src={pokemonshiny ? Imageshiny : actualImage}
+           onClick={() => setPokemonshiny(!pokemonshiny)}
+        />
+       </div>
+       
+                   
+                  
+            
+
+          </div>
+       
+        <div className="card-info">
+        {pokemonshiny && data.name ? <div className="aviso-shiny">shiny</div> : ''}
+          <p>Nome: {data.name}</p>
+          <p>altura: {data.height ? data.height/10  + ' m' : ''}  </p>
+          <p>peso: {data.weight ? data.weight/10  + ' kg' : ''} </p>
+          <p>habilidade: {habilidade ? data.abilities[0].ability.name : ''}</p>
+          {/* Exibindo os tipos do Pokémon */}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
+    
   );
 }
